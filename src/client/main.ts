e9a1e36a361c0a3a43c2d638dd8872352dba6b1f
Alvin Ashiatey <alvinashiatey@gmail.com>
2023-07-26 12:46:21 -0400

    mobile menu

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,7 @@
 import "./scss/main.scss";
-import { setHeaderHeightCss } from "./ts/header";
+import { setHeaderHeightCss, mobileMenu } from "./ts/header";
 
 window.onload = () => {
   setHeaderHeightCss();
+  mobileMenu();
 };
--- src/client/ts/header.ts
+++ src/client/ts/header.ts
@@ -3,6 +3,19 @@ function setHeaderHeightCss() {
   const headerHeight = header.offsetHeight;
   const main = document.querySelector("main") as HTMLElement;
   main.style.paddingBlockStart = headerHeight + "px";
+
+  const title = document.querySelector(".title") as HTMLElement;
+  document.body.style.setProperty("--title-height", title.offsetHeight + "px");
 }
 
-export { setHeaderHeightCss };
+function mobileMenu() {
+  const mobileNavButton = document.getElementById("mobile-nav") as HTMLElement;
+  const mobileNav = document.getElementById("mobile-nav-list") as HTMLElement;
+
+  mobileNavButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    mobileNav.toggleAttribute("data-active");
+  });
+}
+
+export { setHeaderHeightCss, mobileMenu };
